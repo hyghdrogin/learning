eb8da7cb43e6f35d351461bacde4b5a0e34799f9
Hyghdrogin <omokewt@gmail.com>
2023-06-02 14:03:13 +0100

    Generating resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OtpModule } from './otp/otp.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OtpModule],
   controllers: [AppController],
   providers: [AppService],
 })
